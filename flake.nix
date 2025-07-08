2cdab28889e0cc7bf78b4afa77130b94d6e182d8
sid <sid@portuus.de>
2025-07-08 17:07:51 +0200

    removed hm. sops-nix rekey

--- flake.nix
+++ flake.nix
@@ -4,14 +4,8 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-old-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     core.url = "github:sid115/nix-core/develop";
     core.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
     nixos-mailserver.inputs.nixpkgs.follows = "nixpkgs";
@@ -57,7 +51,6 @@
       overlays = import ./overlays { inherit inputs; };
 
       nixosModules = import ./modules/nixos;
-      homeModules = import ./modules/home;
 
       devShells = forAllSystems (
         system:
@@ -75,27 +68,6 @@
             inherit inputs outputs;
           };
           modules = [ ./hosts/portuus ];
-        };
-      };
-
-      homeConfigurations = {
-        "sid@portuus" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
-          modules = [
-            ./users/sid/home
-          ];
-        };
-        "steffen@portuus" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
-          modules = [
-            ./users/steffen/home
-          ];
         };
       };
     };
