aec8c56f56dc776f2d73d8b23c2d7764625b9274
sid <sid@portuus.de>
2025-11-15 18:40:01 +0100

    update flake.lock

--- hosts/portuus/services/headscale.nix
+++ hosts/portuus/services/headscale.nix
@@ -1,19 +1,10 @@
-{ inputs, config, ... }:
+{ inputs, ... }:
 
-let
-  acl = "headscale/acl.hujson";
-in
 {
   imports = [ inputs.core.nixosModules.headscale ];
-
-  environment.etc.${acl} = {
-    inherit (config.services.headscale) user group;
-    text = "{}"; # https://headscale.net/stable/ref/acls/#simple-examples
-  };
 
   services.headscale = {
     enable = true;
     openFirewall = true;
-    settings.policy.path = "/etc/${acl}";
   };
 }
