fca48cf68674374a56a2f49dc5f9afb52a53bbd0
sid <sid@portuus.de>
2025-12-15 12:16:41 +0100

    added max body size to immich

--- hosts/portuus/services/immich.nix
+++ hosts/portuus/services/immich.nix
@@ -8,4 +8,10 @@
     mediaLocation = "/data/immich";
     accelerationDevices = null; # all devices
   };
+
+  services.nginx.virtualHosts."gallery.portuus.de" = {
+    extraConfig = ''
+      client_max_body_size 5G;
+    '';
+  };
 }
